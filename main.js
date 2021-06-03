d684b922aa19e952d5a7651e3f908ac45be27c72
Prince of persia Dastan <60606830+SatanD01@users.noreply.github.com>
2021-06-03 15:05:54 +0500

    changes

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-let firstName;
-firstName = 'Scarpion';
+const $player1 = document.createElement('div');
+$player1.classList.add('player1');
 
+const $p = document.createElement('p');
+$p.innerText = 'Scorpion';
 
-console.log(typeof (firstName));
+$player1.appendChild($p);
+
+const $root = document.querySelector('.root');
+
+$root.appendChild($player1);
+
+const $img = document.createElement ('img');
+
+$img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+$player1.appendChild($img);
